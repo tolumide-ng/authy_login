49487eb2fac171c042f1ea588d198e593f624773
tolumide-ng <tolumideshopein@gmail.com>
2022-10-02 01:45:24 +0200

    Creates FormInput component

---
+++ src/components/FormInput/formInput.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import right from "../../assets/correct.png";
+import wrong from "../../assets/wrong.png";
+import { errors } from "../../utils/errors";
+import styles from "./formInput.module.css";
+
+interface Props {
+    inputValue: string;
+    inputName: string;
+    inputType: string;
+    handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    validatedInput: Record<string, boolean>
+}
+
+export const FormInput = ({inputValue, inputName, inputType, handleChange, validatedInput}: Props) => {
+    return (
+        <div className={styles.inputCont}>
+            <label htmlFor={inputName} className={styles.inputLabel}>
+                {inputName}
+            </label>
+                <input className={styles.input} type={inputType} id={inputType} name={inputName} value={inputValue} onChange={handleChange} />
+
+            <ul className={styles.inputUl}>
+                {Object.entries(validatedInput)?.map(([key, value]) => {
+                    return (
+                        <li key={key} className={styles.inputLi}>
+                            <img className={styles.inputImg} src={value ? right : wrong} alt={value ? "right" : "wrong"} />
+                            <p>{errors[key]}</p>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
