10e5f458d7f814ef03faf24c87348568e7d1f7ff
tolumide-ng <tolumideshopein@gmail.com>
2022-10-02 11:58:32 +0200

    Display the Login component with the page wrapper

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Login } from './components/Login/login';
+import "./assets/styles/_commons.css";
+import { PageWrapper } from './components/PageWrapper/pageWrapper';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <PageWrapper children={<Login />} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
